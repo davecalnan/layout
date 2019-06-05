3bc1c34c7f7a5b12e0f577bfffcf34a3be8f3920
Dave Calnan <d@ve.ie>
2019-06-05 20:12:25 +0100

    Move api to api/src

--- api/src/deploy.js
+++ api/src/deploy.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const React = require('react')
 const ReactDOMServer = require('react-dom/server')
-const { wait } = require('../util')
+const { wait } = require('../../util')
 
 const axios = require('axios')
 const http = axios.create({
