c0039e0dd3da1ef3bbcc2f531586b32c281e38ad
Dave Calnan <d@ve.ie>
2019-06-07 21:44:32 +0100

    Move hardcoded API urls to env variables

--- www/next.config.js
+++ www/next.config.js
@@ -1,3 +1,17 @@
+require('dotenv').config()
+const path = require('path')
+const Dotenv = require('dotenv-webpack')
 const withCSS = require('@zeit/next-css')
 
-module.exports = withCSS()
+module.exports = withCSS({
+  webpack: config => {
+    config.plugins.push(
+      new Dotenv({
+        path: path.join(__dirname, '.env'),
+        systemvars: true
+      })
+    )
+
+    return config
+  }
+})
