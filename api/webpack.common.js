eb6f6ee978d31d61f872f478e36a7b21239afa80
Dave Calnan <d@ve.ie>
2019-06-08 02:46:31 +0100

    Remove webpack warning when building api

--- api/webpack.common.js
+++ api/webpack.common.js
@@ -1,6 +1,9 @@
+const nodeExternals = require('webpack-node-externals')
+
 module.exports = {
   target: 'node',
   entry: './src/index.js',
+  externals: [nodeExternals()],
   output: {
     filename: 'main.js'
   }
