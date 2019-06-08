ca5ab4cd54801c485ca35d9be717906b95154ab3
Dave Calnan <d@ve.ie>
2019-06-08 20:06:44 +0100

    Update preview link

--- www/pages/index.js
+++ www/pages/index.js
@@ -6,7 +6,7 @@ const Homepage = () => (
   <Layout>
     <div className="p-4">
       <P className="mb-4">Wow, this page is incredibly empty.</P>
-      <Link as="/sites/1066427/builder" href="/builder?siteId=1066427">
+      <Link as="/sites/1/builder" href="/builder?siteId=1">
         <a className="text-lg underline text-blue-500">
           Click here to see the proof of concept &rarr;
         </a>
