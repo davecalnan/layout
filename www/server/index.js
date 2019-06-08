4a8af203816569ff27600fd6a19e5b25b6a6fe1e
Dave Calnan <d@ve.ie>
2019-06-08 02:55:31 +0100

    Refactor to es imports

--- www/server/index.js
+++ www/server/index.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const next = require('next')
+import express from 'express'
+import next from 'next'
 
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
