c2a9e8e8deaeabb828a2b42fc86d302f4f192bab
Dave Calnan <d@ve.ie>
2019-06-08 14:02:02 +0100

    Update index.js

--- www/pages/index.js
+++ www/pages/index.js
@@ -1,11 +1,15 @@
 import Link from 'next/link'
 import Layout from '../components/layout'
+import { P } from '../components/typography'
 
 const Homepage = () => (
   <Layout>
     <div className="p-4">
+      <P className="mb-4">
+        Wow, this page is incredibly empty.
+      </P>
       <Link href="/sites/1066427/builder">
-        <a className="text-lg underline text-blue-500">Example</a>
+        <a className="text-lg underline text-blue-500">Click here to see the proof of concept &rarr;</a>
       </Link>
     </div>
   </Layout>
