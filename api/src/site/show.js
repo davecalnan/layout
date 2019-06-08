a8b8566908983358e5058760f5006284f815856f
Dave Calnan <d@ve.ie>
2019-06-08 18:02:02 +0100

    Add 404 error if can't find site

--- api/src/site/show.js
+++ api/src/site/show.js
@@ -1,13 +1,15 @@
+import _ from 'lodash'
 import { stripInternalKeys } from '../helpers'
 
 export default async ({ db, params }, res) => {
   try {
     const { id } = params
-    console.log(id)
 
     const sites = await db.collection('sites')
     const site = await sites.findOne({ id: Number(id) })
-    console.log(stripInternalKeys(site))
+    if (_.isEmpty(site)) {
+      return res.status(404).send(JSON.stringify({ message: `Could not find site with id ${id}.`}))
+    }
 
     res.status(200).send(JSON.stringify(stripInternalKeys(site)))
   } catch (error) {
