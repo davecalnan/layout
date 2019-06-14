f38b72b71d6de87b0bacbca40e600cf528e4739d
Dave Calnan <d@ve.ie>
2019-06-14 23:08:25 +0100

    Fix renderer

--- packages/renderer/src/index.js
+++ packages/renderer/src/index.js
@@ -1,20 +1,20 @@
 import React from 'react'
 import ReactDOMServer from 'react-dom/server'
 
-export const buildComponentTree = (site, options) =>
+export const buildComponentTree = (site, options = {}) =>
   site.components.map(({ id, props }, index) => {
-    const Component = options.server
-      ? require(`@layouthq/sections/dist/${id}`).default
-      : React.lazy(() => import(`@layouthq/sections/dist/${id}`))
+    const Component = options.browser
+      ? React.lazy(() => import(`@layouthq/sections/dist/${id}`))
+      : require(`@layouthq/sections/dist/${id}`).default
 
-      return <Component key={index} {...props} />
-    })
+    return <Component key={index} {...props} />
+  })
 
 export const generateHTML = site =>
   [
     '<!DOCTYPE html><html><head><link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"></head><body>',
     ReactDOMServer.renderToStaticMarkup(
-      buildComponentTree(site, { server: true })
+      buildComponentTree(site)
     ),
     '</body></html>'
   ].join('')
