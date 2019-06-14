0e767ae142c0d1d6dc0986e29a7c1e68aec826aa
Dave Calnan <d@ve.ie>
2019-06-14 23:20:43 +0100

    Make sure browser shadow appears over preview

--- www/components/browser.js
+++ www/components/browser.js
@@ -3,7 +3,7 @@ import Button from './button'
 
 const Browser = ({ url, content }) => (
   <div className="flex flex-col h-full w-full">
-    <header className="flex justify-between shadow bg-gray-300 px-4 py-2">
+    <header className="flex justify-between shadow bg-gray-300 z-0 px-4 py-2">
       <div className="w-20 flex justify-start items-center">
         <div className="w-3 h-3 rounded-full bg-red-500 border border-red-600 mr-2" />
         <div className="w-3 h-3 rounded-full bg-yellow-500 border border-yellow-600 mr-2" />
