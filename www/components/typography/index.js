a9c462663e56e87d2e0ace7af3a9d3ec0fc15cbe
Dave Calnan <d@ve.ie>
2019-06-14 17:07:30 +0100

    Add h4

--- www/components/typography/index.js
+++ www/components/typography/index.js
@@ -5,6 +5,7 @@ const baseHeadingStyles = tw`font-bold leading-none`
 
 export const H1 = styled.h1`${baseHeadingStyles} ${tw`text-5xl`}`
 export const H2 = styled.h2`${baseHeadingStyles} ${tw`text-4xl`}`
-export const H3 = styled.h2`${baseHeadingStyles} ${tw`text-3xl`}`
+export const H3 = styled.h3`${baseHeadingStyles} ${tw`text-3xl`}`
+export const H4 = styled.h4`${baseHeadingStyles} ${tw`text-2xl`}`
 export const P = styled.p`${tw`leading-normal text-lg`}`
 export const Small = styled.small`${tw`block leading-snug text-xs text-gray-700`}`
