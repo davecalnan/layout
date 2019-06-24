4d009f0897e5b317ea67394384f8648acaea91f3
Dave Calnan <d@ve.ie>
2019-06-24 18:37:20 +0100

    Only render one modal at a time

--- packages/www/components/editor.js
+++ packages/www/components/editor.js
@@ -124,66 +124,67 @@ const Editor = ({ site, isLoading, onEdit, className }) => {
     )
   }
 
-  const modalContent = (
-    <>
-      <H2 className="mb-4">Add a section</H2>
-      {availableSections
-        ? (
-          <div
-            style={{
-              display: 'grid',
-              gridTemplateColumns: 'repeat(auto-fill, minmax(10rem, 1fr))'
-            }}
-          >
-            {availableSections.map((section, index) => (
-              <button
-                key={`${index}-${section.name}`}
-                className="bg-white rounded shadow text-left mr-4 mb-4"
-                onClick={() => {
-                  onEdit({
-                    type: ADD_SECTION_TO_PAGE,
-                    target: {
-                      page: currentPage
-                    },
-                    payload: {
-                      id: section.id,
-                      name: section.name,
-                      components: section.defaultComponents,
-                      props: section.defaultProps
-                    }
-                  })
-                  setModalIsOpen(false)
-                }}
-              >
-                <img
-                  src="https://placehold.it/300x200"
-                  className="w-full block rounded-t"
-                />
-                <div className="px-4 py-2">
-                  <P>{toSentenceCase(section.name)}</P>
-                  <Small>{section.description}</Small>
-                </div>
-              </button>
-            ))}
-          </div>
-        ) : (
-          <P>
-            Having trouble finding sections, sorry!
-          </P>
-        )
-      }
-    </>
-  )
+  const determineModalContent = activeSection => {
+    if (!activeSection) {
+      return (
+        <Modal
+          isOpen={modalIsOpen}
+          onRequestClose={() => setModalIsOpen(false)}
+        >
+          <H2 className="mb-4">Add a section</H2>
+          {availableSections ? (
+            <div
+              style={{
+                display: 'grid',
+                gridTemplateColumns:
+                  'repeat(auto-fill, minmax(10rem, 1fr))'
+              }}
+            >
+              {availableSections.map((section, index) => (
+                <button
+                  key={`${index}-${section.name}`}
+                  className="bg-white rounded shadow text-left mr-4 mb-4"
+                  onClick={() => {
+                    onEdit({
+                      type: ADD_SECTION_TO_PAGE,
+                      target: {
+                        page: currentPage
+                      },
+                      payload: {
+                        id: section.id,
+                        name: section.name,
+                        components: section.defaultComponents,
+                        props: section.defaultProps
+                      }
+                    })
+                    setModalIsOpen(false)
+                  }}
+                >
+                  <img
+                    src="https://placehold.it/300x200"
+                    className="w-full block rounded-t"
+                  />
+                  <div className="px-4 py-2">
+                    <P>{toSentenceCase(section.name)}</P>
+                    <Small>{section.description}</Small>
+                  </div>
+                </button>
+              ))}
+            </div>
+          ) : (
+            <P>Having trouble finding sections, sorry!</P>
+          )}
+        </Modal>
+      )
+    }
+
+    return null
+  }
 
   return (
     <div className={className}>
       {determineContent(activeSection)}
-      <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={() => setModalIsOpen(false)}
-      >
-        {modalContent}
-      </Modal>
+      {determineModalContent(activeSection)}
     </div>
   )
 }
