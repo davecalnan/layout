2901aba763fb70732b9b7a5fa81d9e73296b39c9
Dave Calnan <d@ve.ie>
2019-07-05 18:21:39 +0100

    add form component

---
+++ packages/components/src/6.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import PropTypes from '@layouthq/prop-types'
+import { styled } from '@layouthq/renderer'
+import tw from 'tailwind.macro'
+
+const Form = ({ name, action, label, placeholderText, className }) => (
+  <form className={className} action={action} method="post">
+    <input type="hidden" name="form-name" value={name} />
+    {label && <label htmlFor="email">{label}</label>}
+    <div>
+      <input type="email" name="email" placeholder={placeholderText} required />
+      <button type="submit">Submit</button>
+    </div>
+  </form>
+)
+
+Form.propTypes = {
+  ...PropTypes.form,
+  name: PropTypes.string,
+  fields: PropTypes.array,
+  redirectAfterSubmit: PropTypes.string
+}
+
+export default styled(Form)`
+${tw`inline-block max-w-full mb-6 mr-4`}
+${({ theme }) => theme.typography.body}
+
+  & > label {
+    ${tw`block mb-2`}
+  }
+
+  & > div {
+    ${tw`flex`}
+
+    & > input {
+      ${tw`flex-1 rounded-l-full border overflow-x-hidden py-3 px-4 sm:px-6`}
+      border-color: ${({ theme, buttonType }) => theme.colors[buttonType].base};
+
+      &:hover {
+        background-color: ${({ theme }) => theme.colors.secondary.hover};
+      }
+
+      &:focus {
+        ${tw`outline-none shadow-outline`}
+      }
+    }
+
+    & > button {
+      ${tw`text-xs rounded-r-full uppercase tracking-wider py-3 px-4 sm:px-6`}
+      background-color: ${({ theme, buttonType }) =>
+        theme.colors[buttonType].base};
+      border-color: ${({ theme, buttonType }) => theme.colors[buttonType].base};
+      color: ${({ theme, buttonType }) => theme.colors[buttonType].text};
+
+      &:hover {
+        background-color: ${({ theme, buttonType }) =>
+          theme.colors[buttonType].hover};
+      }
+
+      &:focus {
+        ${tw`outline-none shadow-outline`}
+      }
+    }
+  }
+`
