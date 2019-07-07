99f7853b328222cc5b38d8dcdd084023ce098dd3
Dave Calnan <d@ve.ie>
2019-07-07 20:57:04 +0100

    Add redirect to stripe checkout on submit

--- packages/api/src/controllers/form/response.js
+++ packages/api/src/controllers/form/response.js
@@ -4,21 +4,50 @@ export default async (req, res) => {
   try {
     const { db, params, body } = req
     const { domain } = params
-    const referer = req.get('Referer')
+    const referrer = req.get('Referer')
 
     const sites = await db.collection('sites')
     const site = await sites.findOne({ domain })
+
+    const afterSubmit = JSON.parse(body['after-submit'])
 
     const formResponses = await db.collection('formResponses')
     await formResponses.insertOne({
       form: body['form-name'],
       siteId: site.id,
-      referer,
-      body: without(body, 'form-name')
+      referrer,
+      body: without(body, 'form-name', 'after-submit')
     })
 
     res.set('Content-Type', 'text/html')
-    res.status(201).send(`<p>Thank you for submitting.</p><a href="${referer}">Go back &rarr;</a>`)
+
+    res.status(201).send(`
+    <!doctype html>
+<html>
+<head>
+    <title>Thank you for submitting!</title>
+</head>
+<body>
+  <p>Thank you for submitting.</p>
+  ${
+    afterSubmit.type === 'STRIPE_CHECKOUT'
+      ? `<script src="https://js.stripe.com/v3/"></script>
+  <script>
+    const stripe = Stripe('${afterSubmit.payload.token}');
+
+    stripe.redirectToCheckout({
+      items: ${JSON.stringify(afterSubmit.payload.items)},
+      successUrl: '${afterSubmit.payload.successUrl}',
+      cancelUrl: '${afterSubmit.payload.cancelUrl}',
+      customerEmail: '${body.email}'
+    })
+  </script>
+  <p>You will be redirected shortly...</p>`
+      : '<a href="${referrer}">Go back &rarr;</a>'
+  }
+</body>
+</html>
+    `)
   } catch (error) {
     console.error(error)
   }
