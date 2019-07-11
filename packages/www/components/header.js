5f203158b89779f1d21b91f622033c42cce48246
Dave Calnan <d@ve.ie>
2019-07-11 21:33:44 +0100

    Update header.js

--- packages/www/components/header.js
+++ packages/www/components/header.js
@@ -1,9 +1,8 @@
 import Link from 'next/link'
 
-import { H1 } from './typography'
 import Logo from '../assets/layout-logo-color.svg'
 
-const Header = ({ title, content }) => (
+const Header = ({ content }) => (
   <header className="flex justify-between items-center text-blue-900 border-b border-gray-400 bg-white px-4 py-2">
     <div className="max-w-xs w-full">
       <Link href="/">
