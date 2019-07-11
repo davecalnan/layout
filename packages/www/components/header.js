4e20493fecdd9b73555d1e8d2ecf5f84a2e115af
Dave Calnan <d@ve.ie>
2019-07-11 20:30:46 +0100

    Update header.js

--- packages/www/components/header.js
+++ packages/www/components/header.js
@@ -5,16 +5,14 @@ import Logo from '../assets/layout-logo.svg'
 
 const Header = ({ title, content }) => (
   <header className="flex justify-between items-center bg-blue-500 shadow text-white px-4 py-2">
-    <H1 className="flex">
+    <H1 className="flex font-black">
       <Link href="/">
-        <a className="flex font-black text-white">
-          <Logo className="fill-current h-8 mr-2" />
-        </a>
+        <Logo className="fill-current h-8 mr-2" />
       </Link>
       {title && (
         <>
           <span className="font-light mr-2">/</span>
-          {title}
+          <span className="font-black">{title}</span>
         </>
       )}
     </H1>
