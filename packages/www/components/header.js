7baae5b8fb03bb24887da8dffd340762ad319812
Dave Calnan <d@ve.ie>
2019-07-11 20:11:07 +0100

    Remove title from link in header

--- packages/www/components/header.js
+++ packages/www/components/header.js
@@ -5,18 +5,18 @@ import Logo from '../assets/layout-logo.svg'
 
 const Header = ({ title, content }) => (
   <header className="flex justify-between items-center bg-blue-500 shadow text-white px-4 py-2">
-    <H1>
+    <H1 className="flex">
       <Link href="/">
         <a className="flex font-black text-white">
           <Logo className="fill-current h-8 mr-2" />
-          {title && (
-            <>
-              <span className="font-light mr-2">/</span>
-              {title}
-            </>
-          )}
         </a>
       </Link>
+      {title && (
+        <>
+          <span className="font-light mr-2">/</span>
+          {title}
+        </>
+      )}
     </H1>
     {content}
   </header>
