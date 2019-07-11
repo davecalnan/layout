f7a210418377bc4ca1f534157c4458858299c453
Dave Calnan <d@ve.ie>
2019-07-11 15:58:18 +0100

    List user sites on dashboard

--- packages/www/pages/index.js
+++ packages/www/pages/index.js
@@ -1,11 +1,12 @@
 import Router from 'next/router'
 import Link from 'next/link'
 import cookies from 'nookies'
+import axios from 'axios'
 
 import DefaultLayout from '../components/default-layout'
 import { H2 } from '../components/typography'
 
-const Dashboard = () => (
+const Dashboard = ({ sites }) => (
   <DefaultLayout
     title="Dashboard"
     headerContent={
@@ -16,16 +17,20 @@ const Dashboard = () => (
   >
     <div className="p-8">
       <H2 className="mb-4">Your sites</H2>
-      <Link as="/sites/2/builder" href="/builder?siteId=2">
-        <a className="text-lg text-blue-500 underline">
-          Go to your site &rarr;
-        </a>
-      </Link>
+      <ul>
+        {sites.map(({ id, domain }) => (
+          <li className="mb-4">
+            <Link as={`/sites/${id}/builder`} href={`/builder?siteId=${id}`}>
+              <a className="text-lg text-blue-500 underline">{domain} &rarr;</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   </DefaultLayout>
 )
 
-Dashboard.getInitialProps = ctx => {
+Dashboard.getInitialProps = async ctx => {
   const { res } = ctx
   const { token } = cookies.get(ctx)
 
@@ -38,9 +43,16 @@ Dashboard.getInitialProps = ctx => {
     } else {
       Router.push('/login')
     }
+    return {}
   }
 
-  return {}
+  const { data } = await axios.get(`${process.env.API_BASE}/users/me/sites`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+  return { sites: data.data }
 }
 
 export default Dashboard
