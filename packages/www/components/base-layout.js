f37514d711a1fa5461729f2a62a3dc2b0a45c535
Dave Calnan <d@ve.ie>
2019-07-12 16:06:21 +0100

    Remove preview release warning

--- packages/www/components/base-layout.js
+++ packages/www/components/base-layout.js
@@ -1,10 +1,7 @@
 import '../styles/app.css'
 
-import PreviewReleaseWarning from './preview-release-warning'
-
 const BaseLayout = ({ children, className }) => (
   <div className={className}>
-    <PreviewReleaseWarning />
     {children}
   </div>
 )
