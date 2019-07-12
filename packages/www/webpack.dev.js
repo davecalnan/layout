1f36350a8d12cc509d92d429fcddb67a88301155
Dave Calnan <d@ve.ie>
2019-07-12 16:58:47 +0100

    Remove custom www server

--- packages/www/webpack.dev.js
+++
@@ -1,8 +0,0 @@
-const merge = require('webpack-merge')
-const common = require('./webpack.common')
-const NodemonPlugin = require('nodemon-webpack-plugin')
-
-module.exports = merge(common, {
-  mode: 'development',
-  plugins: [new NodemonPlugin()]
-})
