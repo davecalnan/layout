a84fba4214694d9034dad2350a1f4fd8972cb608
Dave Calnan <d@ve.ie>
2019-07-30 17:36:45 +0100

    Add /users/me endpoint

--- packages/api/src/controllers/user/controller.js
+++ packages/api/src/controllers/user/controller.js
@@ -10,11 +10,31 @@ import listSitesHandler from './list-sites'
 
 const controller = Router()
 
-controller.get('/', passport.authenticate('bearer', { session: false }), indexHandler)
+controller.get(
+  '/',
+  passport.authenticate('bearer', { session: false }),
+  indexHandler
+)
 controller.post('/', storeHandler)
-controller.get('/:id', passport.authenticate('bearer', { session: false }), showHandler)
-controller.patch('/:id', passport.authenticate('bearer', { session: false }), updateHandler)
-controller.delete('/:id', passport.authenticate('bearer', { session: false }), deleteHandler)
-controller.get('/:id/sites', passport.authenticate('bearer', { session: false }), listSitesHandler)
+controller.get(
+  '/:id',
+  passport.authenticate('bearer', { session: false }),
+  showHandler
+)
+controller.patch(
+  '/:id',
+  passport.authenticate('bearer', { session: false }),
+  updateHandler
+)
+controller.delete(
+  '/:id',
+  passport.authenticate('bearer', { session: false }),
+  deleteHandler
+)
+controller.get(
+  '/me/sites',
+  passport.authenticate('bearer', { session: false }),
+  listSitesHandler
+)
 
 export { controller }
--- packages/api/src/controllers/user/show.js
+++ packages/api/src/controllers/user/show.js
@@ -1,14 +1,20 @@
 import _ from 'lodash'
 import { withoutInternalKeys } from '@layouthq/util'
 
-export default async ({ db, params }, res) => {
+export default async ({ db, params, user: authUser }, res) => {
   try {
     const { id } = params
+
+    if (id === 'me') {
+      res.status(200).send(JSON.stringify(withoutInternalKeys(authUser)))
+    }
 
     const users = await db.collection('users')
     const user = await users.findOne({ id })
     if (_.isEmpty(user)) {
-      return res.status(404).send(JSON.stringify({ message: `Could not find user with id ${id}.`}))
+      return res
+        .status(404)
+        .send(JSON.stringify({ message: `Could not find user with id ${id}.` }))
     }
 
     res.status(200).send(JSON.stringify(withoutInternalKeys(user)))
